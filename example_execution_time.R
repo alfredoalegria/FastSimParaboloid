cf8a27a396d3b2734ba3643fd20a97e420c244f7
Alfredo Alegría <150471094+alfredoalegria@users.noreply.github.com>
2024-12-08 14:48:32 -0300

    Update example_execution_time.R

--- example_execution_time.R
+++ example_execution_time.R
@@ -1,27 +1,28 @@
-source("/Users/alfredo/Desktop/paraboloid/new/final/sim_algorithm.R")
+source("/sim_algorithm.R")
 set.seed(13)
 
 time=c();
 n=c(100,200,300,400,500);
+N=100;
 
 for(j in 1:length(n)){
+  
+   ang=seq(0,2*pi,l=n[j]);
+   t=seq(0,1,l=n[j])
 
-  ang=seq(0,2*pi,l=n[j]);
-  t=seq(0,1,l=n[j])
+   coord=mesh(ang,t);
+   u=coord$x;
+   v=coord$y;
 
-  coord=mesh(ang,t);
-  u=coord$x;
-  v=coord$y;
+   x=c(v)*cos(c(u));
+   y=c(v)*sin(c(u));
+   z=c(v)^2;
+   sites=cbind(x,y,z);
 
-  x=c(v)*cos(c(u));
-  y=c(v)*sin(c(u));
-  z=c(v)^2;
-  sites=cbind(x,y,z);
+   t1=Sys.time()
+     df=sim_paraboloid_3d(N=N,tau=c(10,100),nu=c(2,2),s=c(1,1,0.6),sites)
+   t2=Sys.time()
 
-  t1=Sys.time()
-  df=sim_paraboloid_3d(N=100,tau=c(10,100),nu=c(2,2),s=c(1,1,0.6),sites)
-  t2=Sys.time()
-
-  time[j]=t2-t1; 
-  print(j)
+   time[j]=t2-t1; 
+   print(j)
 }
