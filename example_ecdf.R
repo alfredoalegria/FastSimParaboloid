3bb2cc6ea7b615673f017c8e10556fb13d97f3c8
Alfredo Alegría <150471094+alfredoalegria@users.noreply.github.com>
2024-12-08 14:43:59 -0300

    Update example_ecdf.R

--- example_ecdf.R
+++ example_ecdf.R
@@ -1,4 +1,4 @@
-source("/Users/alfredo/Desktop/paraboloid/new/final/sim_algorithm.R")
+source("/sim_algorithm.R")
 set.seed(1234)
 
 ang=seq(0,2*pi,l=100);
@@ -14,14 +14,15 @@ z=c(v)^2;
 sites=cbind(x,y,z);
 
 ylab="cumulative distribution function"
+xl=c(-3,3)
+yl=c(0,1)
+
 nrep=100;
 for(j in 1:nrep){
   df=sim_paraboloid_3d(N=100,tau=c(300,300),nu=c(0.5,0.5),s=c(1,1,-0.8),sites)
-  e=ecdf(df$component2);
-  plot(e,col="lightblue",cex=0.1,xlim=c(-3,3),ylim=c(0,1),main="",
-        ylab=ylab,cex.lab=1.3,cex.axis=1.3);
+  e=ecdf(df$component1);
+  plot(e,col="lightblue",cex=0.1,xlim=xl,ylim=yl,,main="",ylab=ylab,cex.lab=1.3,cex.axis=1.3);
   par(new=T)
 }
-curve(pnorm(x),lwd=3,xlim=c(-3,3),ylim=c(0,1),main="",
-                ylab=ylab,cex.lab=1.3,cex.axis=1.3);
+curve(pnorm(x),lwd=3,xlim=xl,ylim=yl,main="",ylab=ylab,cex.lab=1.3,cex.axis=1.3);
 
